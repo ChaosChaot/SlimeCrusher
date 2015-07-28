f19c1c7f8d0d84be8a4600978141ce40b13e123f
ChaosChaot <git@chaoschaot.de>
2015-07-28 21:26:51 +0200

    Rename Mod

--- src/main/java/de/chaoschaot/slimecrasher/lib/Reference.java
+++ src/main/java/de/chaoschaot/slimecrasher/lib/Reference.java
@@ -3,7 +3,7 @@ package de.chaoschaot.slimecrasher.lib;
 
 public class Reference {
    public static final String MODID = "slimecrasher";
-   public static final String MODNAME = "Slime Crasher";
+   public static final String MODNAME = "Slime Crusher";
    public static final String VERSION = "0.3.1";
 
    public static final String CLIENT_PROXY_CLASS = "de.chaoschaot.slimecrasher.proxy.ClientProxy";
